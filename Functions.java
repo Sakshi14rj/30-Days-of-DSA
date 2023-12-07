76aee6ba1a65531b0f26c847155cc75d7d8d3aed
Sakshi <78091453+Sakshi14rj@users.noreply.github.com>
2023-12-07 14:39:06 +0530

    Add files via upload

---
+++ Functions.java
@@ -0,0 +1,55 @@
+import java.util.*;
+public class Functions {
+    public static void main(String[] args) {
+       // max(12 ,34, 33);
+        //max();
+        //oddeven();
+        //istriplet(2, 4, 5);
+    }
+//     static void max(/**int a,int b, int c*/){
+//        //int a =9, b =8 , c=3;
+//        Scanner in = new Scanner(System.in);
+//        int a = in.nextInt();
+//        int b = in.nextInt();
+//        int c = in.nextInt();
+//        System.out.println("The nos by default are "+a+" "+b+" "+c);
+//        if(a>b & a>c){
+//            System.out.println("this is the max number:"+a);
+//        } else if (b>a & b>c) {
+//            System.out.println("this is the max number:"+b);
+//        } else if (c>a & c>b) {
+//            System.out.println("this is the max number:"+c);
+//        }else{
+//            System.out.println("Either two numbers are same or all three numbers are same");
+//        }
+//    }
+
+//    static void oddeven(){
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Enter a number pls ");
+//        int a = in.nextInt();
+////        switch(a%2){
+////            case 1:
+////                System.out.println("odd");
+////                break;
+////            case 0:
+////                System.out.println("even");
+////                break;
+////            default:
+////                System.out.println("error - something feels fishy");
+////        }
+//        if (a%2 == 0){
+//            System.out.println("even");
+//        }else{
+//            System.out.println("odd");
+//        }
+//    }
+//    static void istriplet(int a, int b , int c){
+//        if(Math.sqrt(a*a + b*b)== c){
+//            System.out.println("yes");
+//        }else {
+//            System.out.println("no the three nos don't form a pythagorian triplet");
+//        }
+//    }
+
+}
